345c6edb04f58a8918dfe27825c120b27d9cf2fc
redstone59 <109836856+redstone59@users.noreply.github.com>
2023-10-05 23:43:59 +1000

    remove non-existent file & parsimonius

--- mcmd.py
+++ mcmd.py
@@ -13,14 +13,18 @@ Feature list (maybe):
 
 from arguments import *
 from tokens import *
-from cmdparser import *
 
 import regex as re
+import parsimonious
+import time
+
+START_TIME = time.time()
 
 program_file = getattr(arguments,"in").read()
 compiled_file = getattr(arguments,"out")
 
 # Removing the stuff that the compiler doesn't need, preprocessing
+print("Preprocessing...")
 
 def check_character_position(check:int, positions):
     for x in positions:
@@ -41,14 +45,18 @@ program_trimmed = sub_not_string(r"//.*|/\*[\s\S]*\*/","",program_file) # Removi
 
 macros = re.findall(r"#def.*\n",program_trimmed) # Find all macro definitions in the program
 program_trimmed = sub_not_string(r"#def.*\n","",program_trimmed) # Remove the macro definitions
-for x in macros:
-    definition = x.split()[1:] # Split the macro definition into the macro and it's expansion, trimming the #def
-    program_trimmed = sub_not_string(definition[0]," ".join(definition[1:]),program_trimmed) #Replace all instances of the macro with it's expansion
+for i in range (10): # Allow for macro in macro up to 10 macros deep (this is so a bodge of something)
+    for x in macros:
+        definition = x.split()[1:] # Split the macro definition into the macro and it's expansion, trimming the #def
+        program_trimmed = sub_not_string(definition[0]," ".join(definition[1:]),program_trimmed) #Replace all instances of the macro with it's expansion
 
 program_trimmed = sub_not_string(r"\s*\n\s*","",program_trimmed) # Removing new lines and trimming beginning whitespace
 program_trimmed = sub_not_string(r"\s*,\s*",",",program_trimmed) # Trimming whitespace around commas
 
+# Parsing what is left
+
+
 
 print(program_trimmed)
 
-print("mcmd done")
+print(f"mCmd successfully compiled {getattr(arguments,'in').name} in {(time.time() - START_TIME) // 0.001 * 0.001}s.") # such a silly way to round a number to 2dp
